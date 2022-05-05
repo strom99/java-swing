3f73d0673d5ee5cb21e2aa71359795db4a139c37
strom99 <alissoncc17@gmail.com>
2022-05-05 16:27:54 +0200

    primer commit

---
+++ src/Model/Author.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Author {
+    private String name;
+    private String surname;
+    private String secondSurname;
+    private String country;
+
+}
---
+++ src/Model/Books.java
@@ -0,0 +1,9 @@
+package Model;
+
+public class Books {
+    private String title;
+    private int ISBN;
+    private int NumberOfPages;
+    private String gender;
+    private String author;
+}
